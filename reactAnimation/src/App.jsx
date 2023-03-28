8bf2d05198a9332ea39861a76bacd72dd5716ddf
g4dalcom <g4dalcom@gmail.com>
2023-03-28 21:41:02 +0900

    투표 기능 구현

--- reactAnimation/src/App.jsx
+++ reactAnimation/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
   const [animationParent] = useAutoAnimate();
   const [todoParent] = useAutoAnimate();
+  const [raceParent] = useAutoAnimate();
 
   const [arr, setArr] = useState([
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
@@ -32,8 +33,67 @@ function App() {
     setTodos((todos) => todos.filter((todo) => todo.id !== id));
   };
 
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      name: 'Karina',
+      vote: 0,
+    },
+    {
+      id: 2,
+      name: 'Winter',
+      vote: 0,
+    },
+    {
+      id: 3,
+      name: 'Giselle',
+      vote: 0,
+    },
+    {
+      id: 4,
+      name: 'Ningning',
+      vote: 0,
+    },
+  ]);
+
+  const sortedUsers = users.sort((a, b) =>
+    b.vote.toString().localeCompare(a.vote.toString())
+  );
+
+  const voteUser = (id) => {
+    setUsers((users) =>
+      users.map((user) => {
+        if (user.id === id) {
+          user.vote += 1;
+          console.log(user.vote);
+        }
+        return user;
+      })
+    );
+  };
+
   return (
     <>
+      <div className='w-[400px] mx-auto py-10'>
+        <h3 className='mb-4 text-2xl font-bold'>Raceeee!</h3>
+        <div ref={raceParent} className='grid gap-y-2'>
+          {sortedUsers.map((user) => (
+            <div
+              key={user.id}
+              className='px-4 h-10 text-sm border border-purple-600 text-purple-400 rounded bg-purple-500/10 flex items-center justify-between'
+            >
+              {user.name}
+              <button
+                onClick={() => voteUser(user.id)}
+                className='text-white underline'
+              >
+                Vote({user.vote})
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+
       <div className='w-[400px] mx-auto py-10'>
         <h3 className='mb-4 text-2xl font-bold'>Todo List</h3>
         <form onSubmit={submitHandle} className='flex gap-x-4'>
